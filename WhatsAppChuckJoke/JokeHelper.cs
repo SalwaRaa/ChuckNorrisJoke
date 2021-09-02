13760ca00e5faf4c0984c72260b708fa1338f44c
SalwaRaa <saalwa93@live.se>
2021-09-02 23:55:05 +0200

    Finalized app

---
+++ WhatsAppChuckJoke/JokeHelper.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionApp1
+{
+    class JokeHelper
+    {
+        public static async Task<string> GetJokeAsync()
+        {
+            var joke = "No joke for you.";
+
+            using (var httpClient = new HttpClient())
+            {
+
+                var apiEndpoint = "https://api.chucknorris.io/jokes/random";
+                httpClient.BaseAddress = new Uri(apiEndpoint);
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var responseMessage = await httpClient.GetAsync(apiEndpoint);
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var jsonContent = await responseMessage.Content.ReadAsStringAsync();
+                    dynamic data = JsonConvert.DeserializeObject(jsonContent);
+                    joke = data.value;
+                }
+            }
+
+            return joke;
+        }
+
+    }
+}
